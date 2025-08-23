d1e1564fbd2424d56fedf40082dedfdcbd50da76
kristijanpetronijevic <petronijevick29@gmail.com>
2025-08-23 11:13:21 +0200

    feature: dodat CI tool

---
+++ AssociationRules/html/navtreeindex0.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[0,0],
+"class_main_window.html":[0,0,0],
+"classes.html":[0,1],
+"files.html":[1,0],
+"hierarchy.html":[0,2],
+"index.html":[],
+"mainwindow_8h_source.html":[1,0,0],
+"pages.html":[]
+};
